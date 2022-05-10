7fb35955e1ad90c783b6005eabb48f5f6fff6b4c
baesanga <tkddk1771@gmail.com>
2022-05-10 22:02:55 +0900

    feat : 필요한 필드, get/set 데이터넣기

--- src/main/java/com/bluedigm/notice/NoticeDto.java
+++ src/main/java/com/bluedigm/notice/NoticeDto.java
@@ -3,7 +3,28 @@ package com.bluedigm.notice;
 public class NoticeDto {
 
     // TODO - 이곳에 필요한 데이터를 넣으시요.
+    private int id;
+    private String userId;
     private String title;
+    private String contents;
+    private String regDate;
+    private int hit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = Integer.parseInt(id);
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
     public String getTitle() {
         return title;
@@ -12,4 +33,38 @@ public class NoticeDto {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public void setHit(int hit) {
+        this.hit = hit;
+    }
+
+
+
+
+
+
+
+
+
+
 }
