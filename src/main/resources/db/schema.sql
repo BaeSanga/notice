aa31bef27dbfd5cf9065b7541c7fdb0b59553a7c
baesanga <tkddk1771@gmail.com>
2022-05-12 11:35:09 +0900

    feat : 주석처리

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,11 +1,15 @@
 DROP TABLE tb_notice IF EXISTS;
 
-CREATE TABLE tb_notice (
-      id        INTEGER IDENTITY PRIMARY KEY,
+CREATE TABLE tb_notice (                    --테이블 생성
+      id        INTEGER IDENTITY PRIMARY KEY,           --정수 ID 기본 키
       userId    VARCHAR(30),
       title     VARCHAR(100),
       contents  varchar(500),
-      regDate   datetime,
+      regDate   datetime,                --datetime 형식 : YYYY-MM-DD hh:mm:ss[.nnn]
       hit       INTEGER
 );
 CREATE INDEX tb_notice_id ON tb_notice (id);
+
+
+
+-- char과 varchar 차이점 : char은 고정 길이 문자열 정보, varchar은 가변 길이 문자열 정보
