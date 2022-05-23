d29b36da793192f4e50c26f53cf1a4262f1c76d5
baesanga <tkddk1771@gmail.com>
2022-05-23 16:54:40 +0900

    fix : noticeDao에서 noticeDto로 변수이름 변경

--- src/main/java/com/bluedigm/notice/NoticeMapper.java
+++ src/main/java/com/bluedigm/notice/NoticeMapper.java
@@ -1,14 +1,15 @@
 package com.bluedigm.notice;
 
 import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
 
+//Mapper.xml 과 Mapper.java를 연결해주는 방식은 NameSpace와 각 쿼리문의 id의 조합
 @Mapper
 public interface NoticeMapper {
 
     List<NoticeDto> getNoticeList();
-    NoticeDto getNotice(String id);
-    Integer createNotice(NoticeDto noticeDao);
-    Integer updateNotice(NoticeDto noticeDao);
+    NoticeDto getNotice(Integer id);
+    Integer createNotice(NoticeDto noticeDto);
+    Integer updateNotice(NoticeDto noticeDto);
+    Integer Hit(Integer id);
 }
