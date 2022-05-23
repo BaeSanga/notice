d32df93f170a38e00b5b78c3730da3b3677de85f
baesanga <tkddk1771@gmail.com>
2022-05-23 16:50:18 +0900

    feat : 수정 기능 개발

--- src/main/java/com/bluedigm/notice/NoticeController.java
+++ src/main/java/com/bluedigm/notice/NoticeController.java
@@ -4,41 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
-/**
- * 테이블 정보는 schema.sql과 data.sql을 참고하세요
- * 서버 실행 후 http://localhost:8080/h2-console 을 통해 데이터 베이스 정보 확인이 가능합니다.
- */
-
 @Controller
 public class NoticeController {
 
-    private final NoticeService noticeService;
+    private final NoticeService noticeService;                      
 
-    public NoticeController(NoticeService noticeService) {
+    public NoticeController(NoticeService noticeService) {          //private은 클래스 외부에서 이 변수를 마음대로 접근할 수 없게 돼서
+                                                                    //어떤 객체를 인스턴스로 생성할 때 이 변수들에 일정한값을 주며 생성하기 위해선 생성자이용
         this.noticeService = noticeService;
     }
 
-    // 전체 목록 조회
+    // 전체 목록 조회                                                     //get은 서버에서 어떤 데이터를 가져와서 보여줄때 사용
     @GetMapping("/main")
     public String list(Model model) {
-
-        model.addAttribute("noticelist", noticeService.getNoticeList());
-
+        model.addAttribute("list", noticeService.getNoticeList());      //이름, 넘길값
         return "list";
     }
 
-    // 개별 조회
+    // 상세 조회
     @GetMapping("/main/{id}")                                      //{ } 안에 들어있는 이름과 @PathVariable를 사용한 변수명이 일치해야 한다.
                                                                      //주소 뒤에 {id} 이렇게 id를 받을 때는 @PathVariable을 사용하면 주소의 id로 받음.
-/*
-    @RequestMapping(value = "//main/{id}", method = RequestMethod.GET)
-*/
-    public String viewGetNotice(@PathVariable String id, Model model) {
-
-        model.addAttribute("view", noticeService.getNotice(id));
-        return "/view";
+    public String viewGetNotice(@PathVariable Integer id, Model model) {
+        model.addAttribute("point", noticeService.getNotice(id));
+        noticeService.Hit(id);
+        return "view";
     }
 
     // 등록 페이지 이동
@@ -48,27 +37,23 @@ public class NoticeController {
     }
 
     // 등록
-    @PostMapping("/create")
-    public String createNotice(@ModelAttribute NoticeDto noticeDao) {
-
-        noticeService.createNotice(noticeDao);
-
+    @PostMapping("/create")                                                //post는 서버상의 데이터 값이나 상태를 바꾸기 위해서 사용
+    public String createNotice(@ModelAttribute NoticeDto noticeDto) {
+        noticeService.createNotice(noticeDto);
         return "redirect:/main";
     }
 
     // 수정 페이지 이동
-    @GetMapping("/update")
-    public String updateNoticeForm() {
+    @GetMapping("/update/{id}")
+    public String updateNoticeForm(@PathVariable Integer id, Model model) {
+        model.addAttribute("update", noticeService.getNotice(id));
         return "update";
     }
 
-
     // 수정
     @PostMapping("/update")
-    public String updateNotice(@ModelAttribute NoticeDto noticeDao) {
-
-        // TODO - 이곳에 기능을 넣으세요
-
-        return "redirect:/";
+    public String updateNotice(@ModelAttribute NoticeDto noticeDto) {
+        noticeService.updateNotice(noticeDto);
+        return "redirect:/main";
     }
 }
